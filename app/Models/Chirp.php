b1fe9428ea5f0f0fa34fdfb9e78b86ea5397c809
Eric Hunter <ehunter7@live.com>
2023-02-20 09:03:45 -0800

    added chirp controller, model, migration, policy

---
+++ app/Models/Chirp.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+
+    /**
+     * Get the user that owns the Chirp
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
